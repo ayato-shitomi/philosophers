735cf895ed85135a11d185f8fa4067630b0c3832
Your Name <you@example.com>
2022-07-31 08:35:47 +0900

    one

--- philo_one.c
+++ philo_one.c
@@ -20,14 +20,16 @@ static void	*p_thread_one(void *void_philo)
 
 int	philo_one(t_rules *rules)
 {
-	t_philo	*phi;
+	t_philo		*phi;
+	long long	death;
 
 	phi = rules->philo;
 	rules->first_timestamp = ft_timestamp();
 	if (pthread_create(&(phi[0].thread_id), NULL, p_thread_one, &(phi[0])))
 		return (1);
-	usleep(rules->time_to_deth * 1000);
-	printf("%lli\t", ft_timestamp() - rules->first_timestamp);
+	make_sleep(rules->time_to_deth, rules);
+	death = ft_timestamp();
+	printf("%lli\t", death - rules->first_timestamp);
 	printf("%d died\n", 1);
 	rules->dieded = 1;
 	exit_launcher(rules, phi);
